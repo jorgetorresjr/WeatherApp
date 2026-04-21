29a32642921b42d4fc2b33a715c14c20303ac051
Jorge Luís Torres <jorgetorresjunior175@gmail.com>
2026-04-21 17:48:22 -0300

    Prática 1 – Adicionando telas Login e Home com navegação

---
+++ app/src/main/java/com/example/weatherapp/LoginActivity.kt
@@ -0,0 +1,105 @@
+package com.example.weatherapp
+
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable<MutableState<String>> { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalActivity.current as Activity
+
+    Column(
+        modifier = modifier.padding(24.dp).fillMaxSize(),
+        horizontalAlignment = CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val modifier = modifier.fillMaxWidth(fraction = 0.9f)
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier,
+            onValueChange = { email = it }
+        )
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Row(modifier = modifier.padding(12.dp).fillMaxSize(), horizontalArrangement = Arrangement.Center) {
+            Button( onClick = {
+                Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                activity.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(
+                        FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                )
+            },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+            ) {
+                Text("Login")
+            }
+
+            Spacer(modifier = modifier.size(12.dp))
+
+            Button(
+                onClick = { email = ""; password = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.weatherapp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +29,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
+fun HomePage(modifier: Modifier = Modifier) {
+    val activity = LocalActivity.current as Activity
+
+    Column(
+        modifier = modifier.padding(24.dp).fillMaxSize(),
+        horizontalAlignment = CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Seja Bem-vindo(a)!"
+        )
+
+        Spacer(modifier = Modifier.padding(12.dp))
+
+        Button(
+            onClick = {
+                activity.finish()
+            }
+        ) {
+            Text("Sair")
+        }
     }
 }
+
