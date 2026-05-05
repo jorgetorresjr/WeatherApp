7720ad7152691c5b91c816a976b870aad58099ce
Jorge Luís Torres <jorgetorresjunior175@gmail.com>
2026-05-05 12:24:22 -0300

    Prática 2 – Parte 1

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.example.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,32 +21,6 @@ class MainActivity : ComponentActivity() {
                     HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalActivity.current as Activity
-
-    Column(
-        modifier = modifier.padding(24.dp).fillMaxSize(),
-        horizontalAlignment = CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Seja Bem-vindo(a)!"
-        )
-
-        Spacer(modifier = Modifier.padding(12.dp))
-
-        Button(
-            onClick = {
-                activity.finish()
-            }
-        ) {
-            Text("Sair")
         }
     }
 }
